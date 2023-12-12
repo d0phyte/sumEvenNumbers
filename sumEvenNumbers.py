1a1d365cecc080f9ebe1fea66e2bd47e165714bd
JTcgh <74654976+d0phyte@users.noreply.github.com>
2023-12-12 15:30:19 +0000

    Add files via upload

---
+++ sumEvenNumbers.py
@@ -0,0 +1,18 @@
+startingNum = 0
+
+sum = 0
+
+totalSum = 0
+
+finalNum = int(input("How high do you want to go? Max 1000 "))
+
+for i in range (startingNum,finalNum,2):
+    
+    sum +=2
+    
+    totalSum += sum
+    
+    
+
+print(totalSum)
+
